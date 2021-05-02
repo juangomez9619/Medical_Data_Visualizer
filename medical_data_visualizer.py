222fa5aeab87f96e57f6cd48cc2c78e8eb11ef67
juangomez9619 <judgomezb@correo.udistrital.edu.co>
2021-05-01 21:14:17 -0500

    Categorical Plot draw and test OK.

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,28 +4,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # Add 'overweight' column
-df['overweight'] = None
+def overweight_calculator(weight, height):
+    BMI = weight / ((height/100))**2
+    if BMI > 25:
+        return 1
+    return 0
+df['overweight'] = df.apply(lambda row: overweight_calculator(row.weight, row.height),
+axis = 1)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
-
+def normalize_cholesterol_gluc(variable):
+    if variable == 1:
+        return 0
+    return 1
+df['cholesterol'] = df.apply(lambda row: normalize_cholesterol_gluc(row['cholesterol']), axis=1)
+df['gluc'] = df.apply(lambda row: normalize_cholesterol_gluc(row['gluc']), axis=1)
 
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
-
-
+    df_cat =  pd.melt(df,id_vars=['cardio'],value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
-
+    df_cat = pd.DataFrame(df_cat.groupby(by = ['cardio','variable','value'])['value'].count())
+    df_cat.columns= ['total']
+    df_cat.reset_index(inplace=True)
     # Draw the catplot with 'sns.catplot()'
-
-
-
+    graph = sns.catplot(x="variable", y="total", hue="value",col="cardio", data=df_cat, kind='bar')
     # Do not modify the next two lines
+    fig = graph.fig
     fig.savefig('catplot.png')
     return fig
 
